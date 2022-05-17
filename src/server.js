c72ebab96e527bbffaa0fad6b6a4078dfeecf7bf
leobusta <leobusta@gmail.com>
2022-05-17 18:34:42 -0500

    first commit

---
+++ src/server.js
@@ -0,0 +1,4 @@
+module.exports =  {
+    auth_api_url: 'https://leo-misionticgr33.herokuapp.com',
+    account_api_url: 'https://misiontic-account-gr34.herokuapp.com'
+}
---
+++ src/typeDefs/index.js
@@ -0,0 +1,6 @@
+const accountTypeDefs = require("./account_type_defs")
+const authTypeDefs = require("./auth_type_defs")
+const transactionTypeDefs = require("./transaction_type_defs")
+
+module.exports = [authTypeDefs, accountTypeDefs, transactionTypeDefs]
+
